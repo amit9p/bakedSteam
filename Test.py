218dfb4eb29457b8d73c29425eed34715d46d989
amit9p <amit_prasad@live.com>
2024-05-10 11:51:30 -0500

    Update Test.py

--- Test.py
+++ Test.py
@@ -1,3 +1,10 @@
 print("hello world")
 
 fuucjvivjvjvjvjv kzitxkxitdxkfxkgxkx
+# Example string
+s = "metro2-all+transunion"
+
+# Split the string based on '+' and take the second part
+key = s.split('+')[1]
+
+print(key)  # Output will be 'transunion'
