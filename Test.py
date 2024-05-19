2194b4ee9f628aaa549754269b9fa7920b4cfd96
amit9p <amit_prasad@live.com>
2024-05-19 00:07:03 -0500

    Update Test.py

--- Test.py
+++ Test.py
@@ -13,3 +13,20 @@ def read_config(self):
         print("Error parsing configuration file:", exc)
         return None
     return configfile
+
+
+import yaml
+
+class Configs:
+    def __init__(self):
+        self.yaml_file = '/path/to/your/app_config.yaml'  # Update the path as necessary
+
+    def read_config(self):
+        print("Reading configuration from YAML file...")
+        with open(self.yaml_file, 'r') as file:
+            configfile = yaml.load(file, Loader=yaml.SafeLoader)
+            print("Configuration loaded successfully.")
+        return configfile
+
+    def test(self):
+        print("hello world")
