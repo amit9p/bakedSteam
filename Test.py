faa8aac57a0c8dd8590add6945445a022aef5d65
amit9p <amit_prasad@live.com>
2024-05-19 00:05:14 -0500

    Update Test.py

--- Test.py
+++ Test.py
@@ -1,19 +1,15 @@
 
 
-[tool.black]
-line-length = 120
-include = '\.pyi?$'
-exclude = '''
-/(
-    \.eggs
-  | \.git
-  | \.hg
-  | \.mypy_cache
-  | \.tox
-  | \.venv
-  | _build
-  | buck-out
-  | build
-  | dist
-)/
-'''
+def read_config(self):
+    print("Reading configuration from YAML file...")
+    try:
+        with open(self.yaml_file, 'r') as file:
+            configfile = yaml.load(file, Loader=yaml.SafeLoader)
+            print("Configuration loaded successfully.")
+    except FileNotFoundError:
+        print("Configuration file not found.")
+        return None
+    except yaml.YAMLError as exc:
+        print("Error parsing configuration file:", exc)
+        return None
+    return configfile
