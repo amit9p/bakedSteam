c5c7e67523924b336a3ec6dea5df9e594f85afc8
amit9p <amit_prasad@live.com>
2024-06-02 12:54:09 -0500

    Update Test2.py

--- Test2.py
+++ Test2.py
@@ -3,13 +3,14 @@
 import boto3
 import pandas as pd
 import io
+from botocore.config import Config
 
 # Define AWS credentials and bucket/file information
 aws_access_key_id = 'your_access_key_id'
 aws_secret_access_key = 'your_secret_access_key'
 aws_session_token = 'your_session_token'  # Optional, only if you are using temporary credentials
 bucket_name = 'your_bucket_name'
-file_key = 'path/to/your/file.parquet'
+file_key = 'path/to/your/file.json'
 
 # Initialize a session using your credentials
 session = boto3.Session(
@@ -18,15 +19,24 @@ session = boto3.Session(
     aws_session_token=aws_session_token  # Optional
 )
 
-# Use the session to create an S3 client
-s3_client = session.client('s3')
+# Config to skip SSL verification
+config = Config(
+    retries = {
+        'max_attempts': 10,
+        'mode': 'standard'
+    },
+    ssl_verify=False
+)
 
-# Get the Parquet file from S3
+# Use the session to create an S3 client with the specified config
+s3_client = session.client('s3', config=config)
+
+# Get the JSON file from S3
 obj = s3_client.get_object(Bucket=bucket_name, Key=file_key)
 data = obj['Body'].read()
 
-# Read the Parquet file into a Pandas DataFrame
-df = pd.read_parquet(io.BytesIO(data))
+# Read the JSON file into a Pandas DataFrame
+df = pd.read_json(io.BytesIO(data))
 
 # Display the DataFrame
 print(df)
