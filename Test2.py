e2a91443864d5baf62a3bba6a726ffb18c19c960
amit9p <amit_prasad@live.com>
2024-06-11 15:54:42 -0500

    Update Test2.py

--- Test2.py
+++ Test2.py
@@ -1,32 +1,79 @@
 
+The error "No module named chardet" in AWS Lambda indicates that the `chardet` module is not included in your Lambda function's deployment package. To resolve this, you need to add the `chardet` module to your Lambda function's deployment package.
 
-[packages]
-urllib3 = ">=2.0.1,<3"
+Here are the steps to do that:
 
+1. **Create a directory for your Lambda function**:
+    ```sh
+    mkdir my_lambda_function
+    cd my_lambda_function
+    ```
 
-from pyspark.sql import SparkSession
+2. **Create a virtual environment**:
+    ```sh
+    python3 -m venv v-env
+    source v-env/bin/activate
+    ```
 
-# Create Spark session
-spark = SparkSession.builder.appName("DataFrame Join Example").getOrCreate()
+3. **Install the `chardet` module**:
+    ```sh
+    pip install chardet
+    ```
 
-# Sample data for DataFrame 1
-data1 = [("val1", "123", "USTAXID"), ("val2", "456", "PAN")]
-df1 = spark.createDataFrame(data1, ["value", "account_id", "tokenization_type"])
+4. **Create a deployment package**:
+    ```sh
+    mkdir package
+    cd package
+    cp -r ../v-env/lib/python3.*/site-packages/* .
+    cp -r ../v-env/lib64/python3.*/site-packages/* .
+    ```
 
-# Sample data for DataFrame 2
-data2 = [("val3", "123", "USTAXID"), ("val4", "456", "PAN")]
-df2 = spark.createDataFrame(data2, ["value", "account_id", "tokenization_type"])
+5. **Add your Lambda function code to the package**:
+    ```sh
+    cp ../my_lambda_function.py .
+    ```
 
-# Rename the 'value' column in df1 and df2 to make them distinguishable
-df1_renamed = df1.withColumnRenamed("value", "value_df1")
-df2_renamed = df2.withColumnRenamed("value", "value_df2")
+6. **Zip the package**:
+    ```sh
+    zip -r9 ../my_lambda_function.zip .
+    cd ..
+    ```
 
-# Perform an inner join on 'account_id' and 'tokenization_type'
-df_joined = df1_renamed.join(df2_renamed, ["account_id", "tokenization_type"], "inner")
+7. **Upload the deployment package to AWS Lambda**:
+    - Go to the AWS Lambda console.
+    - Select your function.
+    - Under "Function code," select "Upload from" and then "ZIP file."
+    - Upload the `my_lambda_function.zip` file.
 
-# Show the resulting DataFrame
-df_joined.show()
+Alternatively, you can use AWS Lambda Layers to include the `chardet` module. Here are the steps:
 
-# Optionally, if you want to rearrange the columns in specific order
-df_final = df_joined.select("value_df1", "value_df2", "account_id", "tokenization_type")
-df_final.show()
+1. **Create a layer directory**:
+    ```sh
+    mkdir python
+    cd python
+    ```
+
+2. **Install the `chardet` module in the layer directory**:
+    ```sh
+    pip install chardet -t .
+    ```
+
+3. **Zip the layer directory**:
+    ```sh
+    zip -r9 ../chardet_layer.zip .
+    cd ..
+    ```
+
+4. **Create a new Lambda layer**:
+    - Go to the AWS Lambda console.
+    - Select "Layers" from the left-hand menu.
+    - Create a new layer.
+    - Upload the `chardet_layer.zip` file.
+
+5. **Add the layer to your Lambda function**:
+    - Go to your Lambda function in the console.
+    - In the "Layers" section, click "Add a layer."
+    - Select "Custom layers."
+    - Choose the layer you created and add it to your function.
+
+Using these methods, the `chardet` module should be available to your AWS Lambda function.
