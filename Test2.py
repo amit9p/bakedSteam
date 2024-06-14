9eacd348b67801b99d53c79b77fbde8ee6cb7a26
amit9p <amit_prasad@live.com>
2024-06-14 17:38:34 -0500

    Update Test2.py

--- Test2.py
+++ Test2.py
@@ -1,3 +1,6 @@
 
-docker run --rm -v $(pwd):/var/task public.ecr.aws/lambda/python:3.8 pip install -r requirements.txt -t .
-docker run --rm -v $(pwd):/var/task lambci/lambda:build-python3.8 pip install -r requirements.txt -t .
+{
+  "registry-mirrors": [
+    "https://artifactory-dockerhub.cloud.capitalone.com"
+  ]
+}
