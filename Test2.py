3b87223434e6494ed2fe26356f255c90d7bb720a
amit9p <amit_prasad@live.com>
2024-06-14 18:14:05 -0500

    Update Test2.py

--- Test2.py
+++ Test2.py
@@ -1,5 +1,19 @@
 
 
+
+# hello_world/app.py
+import json
+
+def lambda_handler(event, context):
+    return {
+        'statusCode': 200,
+        'body': json.dumps('Hello, World!')
+    }
+
+
+
+
+
 AWSTemplateFormatVersion: '2010-09-09'
 Transform: AWS::Serverless-2016-10-31
 Resources:
