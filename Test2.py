b2f64ecda695b1131b2feeb9adf8e88f19e3c0df
amit9p <amit_prasad@live.com>
2024-06-14 17:06:28 -0500

    Update Test2.py

--- Test2.py
+++ Test2.py
@@ -1,3 +1,3 @@
 
-
+docker run --rm -v $(pwd):/var/task public.ecr.aws/lambda/python:3.8 pip install -r requirements.txt -t .
 docker run --rm -v $(pwd):/var/task lambci/lambda:build-python3.8 pip install -r requirements.txt -t .
