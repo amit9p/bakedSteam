75bb82938be8f21ee63f733c2cb46325f4f14f9d
amit9p <amit_prasad@live.com>
2024-06-14 18:11:33 -0500

    Update Test2.py

--- Test2.py
+++ Test2.py
@@ -1,6 +1,21 @@
 
-{
-  "registry-mirrors": [
-    "https://artifactory-dockerhub.cloud.capitalone.com"
-  ]
-}
+
+AWSTemplateFormatVersion: '2010-09-09'
+Transform: AWS::Serverless-2016-10-31
+Resources:
+  HelloWorldFunction:
+    Type: AWS::Serverless::Function
+    Properties:
+      Handler: hello_world.app.lambda_handler
+      Runtime: python3.8
+      CodeUri: hello_world/
+      MemorySize: 128
+      Timeout: 3
+      Policies:
+        - AWSLambdaBasicExecutionRole
+      Events:
+        HelloWorldApi:
+          Type: Api
+          Properties:
+            Path: /hello
+            Method: get
