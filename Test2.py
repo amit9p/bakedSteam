acabd2fa020b58d7ca548fdfb2f3373796b2047a
amit9p <amit_prasad@live.com>
2024-06-18 13:48:21 -0500

    Update Test2.py

--- Test2.py
+++ Test2.py
@@ -1,44 +1,40 @@
 
+from pyspark.sql.types import StructType, StructField, StringType, LongType
+from ecbr_logging import logging
 
-aws glue create-job \
-  --name your-job-name \
-  --role your-iam-role \
-  --command '{"Name": "glueetl", "ScriptLocation": "s3://your-bucket/path/to/your-script.py"}' \
-  --default-arguments '{"--extra-jars":"s3://your-bucket/path/to/your-jar1.jar,s3://your-bucket/path/to/your-jar2.jar"}'
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)
 
+# Declare string variables at the top
+ACCOUNT_ID = 'account_id'
+RUN_ID = 'run_id'
+SEGMENT = 'segment'
+ATTRIBUTE = 'attribute'
+VALUE = 'value'
+ROW_SEQUENCE = 'row_sequence'
+COLUMN_SEQUENCE = 'column_sequence'
+SITE_TYPE = 'site_type'
+BUSINESS_DATE = 'business_date'
+INDEX_LEVEL = '__index_level_0__'
+TOKENIZATION_TYPE = 'tokenization_type'
 
-
-docker pull public.ecr.aws/sam/emulation-python3.8
-
-# hello_world/app.py
-import json
-
-def lambda_handler(event, context):
-    return {
-        'statusCode': 200,
-        'body': json.dumps('Hello, World!')
-    }
-
-
-
-
-
-AWSTemplateFormatVersion: '2010-09-09'
-Transform: AWS::Serverless-2016-10-31
-Resources:
-  HelloWorldFunction:
-    Type: AWS::Serverless::Function
-    Properties:
-      Handler: hello_world.app.lambda_handler
-      Runtime: python3.8
-      CodeUri: hello_world/
-      MemorySize: 128
-      Timeout: 3
-      Policies:
-        - AWSLambdaBasicExecutionRole
-      Events:
-        HelloWorldApi:
-          Type: Api
-          Properties:
-            Path: /hello
-            Method: get
+def read_parquet_file(spark, path):
+    try:
+        schema = StructType([
+            StructField(ACCOUNT_ID, StringType(), True),
+            StructField(RUN_ID, StringType(), True),
+            StructField(SEGMENT, StringType(), True),
+            StructField(ATTRIBUTE, StringType(), True),
+            StructField(VALUE, StringType(), True),
+            StructField(ROW_SEQUENCE, LongType(), True),
+            StructField(COLUMN_SEQUENCE, LongType(), True),
+            StructField(SITE_TYPE, StringType(), True),
+            StructField(BUSINESS_DATE, StringType(), True),
+            StructField(INDEX_LEVEL, LongType(), True),
+            StructField(TOKENIZATION_TYPE, StringType(), True),
+        ])
+        df = spark.read.schema(schema).parquet(path)
+        df.printSchema()
+        return df
+    except Exception as e:
+        logger.error(e)
