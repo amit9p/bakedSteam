d69ad3dc583257ea2316ce875fa023d772facbcb
amit9p <amit_prasad@live.com>
2024-07-01 15:25:15 -0500

    Update Test2.py

--- Test2.py
+++ Test2.py
@@ -1,7 +1,6 @@
 
-from unittest.mock import patch, Mock
-import pytest
-from utils.credentials_utils import get_cli_creds
+from unittest.mock import Mock, patch
+import json
 
 @patch("utils.config_reader.load_config", return_value=None)
 @patch("secret_sauce.IamClient")
@@ -18,24 +17,24 @@ def test_get_cli_creds_success_non_qa(mock_add_auth, mock_post, mock_iam_client_
             "CLIENT_SECRET_PATH": "client_secret_path"
         }
     }
-    
     mock_load_config.return_value = mock_chamber_config
+
     mock_response = Mock()
     mock_response.status_code = 200
-    mock_response.json.return_value = {"auth": {"client_token": "mock_token"}}
+    mock_response.json.return_value = {"token": "mock_token"}
     mock_post.return_value = mock_response
 
     # Create a mock instance for IamClient and its methods
     mock_iam_client = Mock()
     mock_iam_client.get_secret_from_path.side_effect = lambda path, secret_key: f"{secret_key}_value"
     mock_iam_client.get_token.return_value = "mock_token"
-    mock_iam_client.request_vault = Mock(return_value={"auth": {"client_token": "mock_token"}})
+    mock_iam_client.request_vault = Mock()
 
     # Ensure that initializing IamClient doesn't trigger real network calls
     mock_iam_client_class.return_value = mock_iam_client
 
     env = "prod"
-    
+
     result = get_cli_creds("chamber_config", env)
 
     mock_load_config.assert_called_once_with("CONFIGS_CHAMBER", env)
@@ -44,7 +43,7 @@ def test_get_cli_creds_success_non_qa(mock_add_auth, mock_post, mock_iam_client_
     )
     mock_iam_client.get_secret_from_path.assert_any_call(path="client_id_path", secret_key="client_id")
     mock_iam_client.get_secret_from_path.assert_any_call(path="client_secret_path", secret_key="client_secret")
-
+    
     assert result == {
         "client_id": "client_id_value",
         "client_secret": "client_secret_value"
