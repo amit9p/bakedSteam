672c51ce48c9ca59931b55d07ee20772ff0673f3
amit9p <amit_prasad@live.com>
2024-07-02 15:37:44 -0500

    Update Test2.py

--- Test2.py
+++ Test2.py
@@ -1,4 +1,12 @@
 
+
+CREATE OR REPLACE TASK my_daily_task
+  WAREHOUSE = my_warehouse
+  SCHEDULE = 'USING CRON 0 0 * * * UTC'  -- Executes daily at midnight UTC
+AS
+  INSERT INTO my_table_archive SELECT * FROM my_table WHERE event_date = CURRENT_DATE();
+
+
 CREATE MATERIALIZED VIEW my_materialized_view
 AS
 SELECT column1, SUM(column2) FROM my_table GROUP BY column1;
