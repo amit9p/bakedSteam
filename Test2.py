dbae75b8292a57737b09d67acafd513950c3f7a5
amit9p <amit_prasad@live.com>
2024-07-02 09:56:56 -0500

    Update Test2.py

--- Test2.py
+++ Test2.py
@@ -1,7 +1,5 @@
 
-
 import pytest
-import os
 from unittest.mock import patch, mock_open
 import yaml
 
@@ -26,40 +24,11 @@ def mock_open_yaml():
         yield mock_file
 
 @pytest.fixture
-def mock_yaml_safe_load():
-    with patch("yaml.safe_load", return_value=yaml.safe_load(sample_yaml_content)) as mock_yaml:
-        yield mock_yaml
-
-@pytest.fixture
 def mock_os_path():
     with patch("os.path.dirname", return_value="/path/to/config"):
         with patch("os.path.abspath", return_value="/path/to/config"):
             with patch("os.path.join", return_value="/path/to/config/app_config.yaml"):
                 yield
-
-def test_load_config_success(mock_open_yaml, mock_yaml_safe_load, mock_os_path):
-    result = load_config("dev", "qa")
-    expected_config = yaml.safe_load(sample_yaml_content)["dev"]
-    assert result == {"env_config": expected_config}
-
-def test_load_config_file_not_found(mock_os_path):
-    with patch("builtins.open", side_effect=FileNotFoundError):
-        result = load_config("dev", "qa")
-        assert result is None
-
-def test_load_config_yaml_error(mock_open_yaml, mock_os_path):
-    with patch("yaml.safe_load", side_effect=yaml.YAMLError):
-        result = load_config("dev", "qa")
-        assert result is None
-
-def test_load_config_key_error(mock_open_yaml, mock_os_path):
-    incomplete_yaml_content = """
-    dev:
-      VAULT_ROLE: dummy_vault_role
-    """
-    with patch("builtins.open", mock_open(read_data=incomplete_yaml_content)):
-        result = load_config("dev", "qa")
-        assert result is None
 
 def test_load_config_generic_exception(mock_open_yaml, mock_os_path):
     with patch("yaml.safe_load", side_effect=Exception):
