2ebd824867db029dcd9936403563b8aea5e40327
amit9p <amit_prasad@live.com>
2024-07-12 13:51:37 -0500

    Update Test2.py

--- Test2.py
+++ Test2.py
@@ -1,6 +1,6 @@
 
 from pyspark.sql import SparkSession
-from pyspark.sql.functions import col
+from pyspark.sql.functions import when, col
 
 # Initialize Spark session
 spark = SparkSession.builder.appName("UpdateDF").getOrCreate()
@@ -13,14 +13,23 @@ df2 = spark.read.parquet("/mnt/data/file-ry7at8nK7N1dS8RxRjahVLn8")
 df2 = df2.withColumnRenamed("ssn", "social_security_number") \
          .withColumnRenamed("pan", "consumer_account_number")
 
-# Update df1 based on df2 values
+# Create a temporary view of df2
+df2.createOrReplaceTempView("df2")
+
+# Update df1 based on df2 values and tokenization_type
 df1_updated = df1 \
-    .join(df2, df1.account_id == df2.account_id, "left") \
     .withColumn("value", 
-                when(df1.attribute == "Social Security Number", df2.social_security_number)
-                .when(df1.attribute == "Consumer Account Number", df2.consumer_account_number)
-                .otherwise(df1.value)) \
-    .select(df1.columns)
+                when((df1.attribute == "Social Security Number") & (df1.tokenization_type == "USTAXID"), 
+                     col("social_security_number")).when((df1.attribute == "Consumer Account Number") & (df1.tokenization_type == "PAN"), 
+                     col("consumer_account_number")).otherwise(df1.value))
+
+# Join df1 with df2 based on the updated value column
+df1_final = df1_updated.alias("df1") \
+    .join(df2.alias("df2"), 
+          (col("df1.value") == col("df2.social_security_number")) | 
+          (col("df1.value") == col("df2.consumer_account_number")), 
+          "left") \
+    .select(col("df1.*"), col("df2.social_security_number"), col("df2.consumer_account_number"))
 
 # Show the updated DataFrame
-df1_updated.show()
+df1_final.show()
