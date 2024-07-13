83595e328c6a376984cd0fae2aafb4543a5e9410
amit9p <amit_prasad@live.com>
2024-07-13 15:06:47 -0500

    Update Test2.py

--- Test2.py
+++ Test2.py
@@ -10,41 +10,31 @@ spark = SparkSession.builder.appName("DataFrameReplacement").getOrCreate()
 df1 = spark.read.parquet("/mnt/data/file-U4qkPznEyieGeGhGQns07nx7")
 df2 = spark.read.parquet("/mnt/data/file-C5CTELwnzCKFOajIoqYsj7rk")
 
-# Filter relevant rows from df1 for Social Security Number and Consumer Account Number
+# Filter and rename columns for SSN
 df1_ssn = df1.filter((df1["attribute"] == "Social Security Number") & (df1["tokenization_type"] == "USTAXID")) \
-             .select("value", "attribute", "tokenization_type").withColumnRenamed("value", "ssn_value")
+             .select(col("value").alias("ssn_value"), col("attribute").alias("ssn_attribute"), col("tokenization_type").alias("ssn_tokenization_type"))
+
+# Join and replace SSN values
+df2_with_ssn = df2.join(df1_ssn, 
+                        (df2["attribute"] == df1_ssn["ssn_attribute"]) & (df2["tokenization_type"] == df1_ssn["ssn_tokenization_type"]), 
+                        "left") \
+                  .withColumn("value", 
+                              when((col("attribute") == "Social Security Number") & (col("tokenization_type") == "USTAXID"), 
+                                   col("ssn_value")).otherwise(col("value"))) \
+                  .drop("ssn_attribute").drop("ssn_tokenization_type").drop("ssn_value")
+
+# Filter and rename columns for PAN
 df1_pan = df1.filter((df1["attribute"] == "Consumer Account Number") & (df1["tokenization_type"] == "PAN")) \
-             .select("value", "attribute", "tokenization_type").withColumnRenamed("value", "pan_value")
+             .select(col("value").alias("pan_value"), col("attribute").alias("pan_attribute"), col("tokenization_type").alias("pan_tokenization_type"))
 
-# Join df2 with df1_ssn for Social Security Number replacement
-df2_with_ssn = df2.alias("df2").join(df1_ssn.alias("df1_ssn"), 
-                                     (col("df2.attribute") == col("df1_ssn.attribute")) & 
-                                     (col("df2.tokenization_type") == col("df1_ssn.tokenization_type")), 
-                                     "left")
-
-# Update the value column for Social Security Number
-df2_with_ssn = df2_with_ssn.withColumn("value", 
-                                       when((col("df2.attribute") == "Social Security Number") & 
-                                            (col("df2.tokenization_type") == "USTAXID"), 
-                                            col("df1_ssn.ssn_value")).otherwise(col("df2.value")))
-
-# Drop ambiguous columns from the join
-df2_with_ssn = df2_with_ssn.drop("df1_ssn.attribute").drop("df1_ssn.tokenization_type").drop("df1_ssn.ssn_value")
-
-# Join the intermediate result with df1_pan for Consumer Account Number replacement
-df2_final = df2_with_ssn.alias("df2").join(df1_pan.alias("df1_pan"), 
-                                           (col("df2.attribute") == col("df1_pan.attribute")) & 
-                                           (col("df2.tokenization_type") == col("df1_pan.tokenization_type")), 
-                                           "left")
-
-# Update the value column for Consumer Account Number
-df2_final = df2_final.withColumn("value", 
-                                 when((col("df2.attribute") == "Consumer Account Number") & 
-                                      (col("df2.tokenization_type") == "PAN"), 
-                                      col("df1_pan.pan_value")).otherwise(col("df2.value")))
-
-# Drop ambiguous columns from the join
-df2_final = df2_final.drop("df1_pan.attribute").drop("df1_pan.tokenization_type").drop("df1_pan.pan_value")
+# Join and replace PAN values
+df2_final = df2_with_ssn.join(df1_pan, 
+                              (df2_with_ssn["attribute"] == df1_pan["pan_attribute"]) & (df2_with_ssn["tokenization_type"] == df1_pan["pan_tokenization_type"]), 
+                              "left") \
+                        .withColumn("value", 
+                                    when((col("attribute") == "Consumer Account Number") & (col("tokenization_type") == "PAN"), 
+                                         col("pan_value")).otherwise(col("value"))) \
+                        .drop("pan_attribute").drop("pan_tokenization_type").drop("pan_value")
 
 # Show the updated dataframe
 df2_final.show()
