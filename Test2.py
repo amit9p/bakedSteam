808259fc4f70f6447736a5e3b94b7ea6734b3e9c
amit9p <amit_prasad@live.com>
2024-07-13 14:19:51 -0500

    Update Test2.py

--- Test2.py
+++ Test2.py
@@ -1,6 +1,6 @@
 
 from pyspark.sql import SparkSession
-from pyspark.sql.functions import col
+from pyspark.sql.functions import col, when
 
 # Initialize Spark session
 spark = SparkSession.builder.appName("DataFrameReplacement").getOrCreate()
@@ -9,50 +9,27 @@ spark = SparkSession.builder.appName("DataFrameReplacement").getOrCreate()
 df1 = spark.read.parquet("/path/to/df1.parquet")
 df2 = spark.read.parquet("/path/to/df2.parquet")
 
-# Create temporary views for SQL queries
-df1.createOrReplaceTempView("df1")
-df2.createOrReplaceTempView("df2")
-
 # Update df2 with values from df1 where attribute is Social Security Number and tokenization_type is USTAXID
-df2_updated_ssn = spark.sql("""
-    SELECT 
-        df2.account_id,
-        df2.attribute,
-        CASE
-            WHEN df2.attribute = 'Social Security Number' AND df2.tokenization_type = 'USTAXID'
-            THEN df1.value
-            ELSE df2.value
-        END as value,
-        df2.tokenization_type
-    FROM df2
-    LEFT JOIN df1
-    ON df2.attribute = df1.attribute
-    AND df2.tokenization_type = df1.tokenization_type
-    AND df2.attribute = 'Social Security Number'
-    AND df2.tokenization_type = 'USTAXID'
-""")
-
-# Create a temporary view for df2_updated_ssn
-df2_updated_ssn.createOrReplaceTempView("df2_updated_ssn")
+df2_updated = df2.withColumn(
+    "value",
+    when(
+        (df2["attribute"] == "Social Security Number") & (df2["tokenization_type"] == "USTAXID"),
+        df1.filter(
+            (df1["attribute"] == "Social Security Number") & (df1["tokenization_type"] == "USTAXID")
+        ).select("value").first()[0]
+    ).otherwise(df2["value"])
+)
 
 # Update df2 with values from df1 where attribute is Consumer Account Number and tokenization_type is PAN
-df2_final = spark.sql("""
-    SELECT 
-        df2_updated_ssn.account_id,
-        df2_updated_ssn.attribute,
-        CASE
-            WHEN df2_updated_ssn.attribute = 'Consumer Account Number' AND df2_updated_ssn.tokenization_type = 'PAN'
-            THEN df1.value
-            ELSE df2_updated_ssn.value
-        END as value,
-        df2_updated_ssn.tokenization_type
-    FROM df2_updated_ssn
-    LEFT JOIN df1
-    ON df2_updated_ssn.attribute = df1.attribute
-    AND df2_updated_ssn.tokenization_type = df1.tokenization_type
-    AND df2_updated_ssn.attribute = 'Consumer Account Number'
-    AND df2_updated_ssn.tokenization_type = 'PAN'
-""")
+df2_final = df2_updated.withColumn(
+    "value",
+    when(
+        (df2_updated["attribute"] == "Consumer Account Number") & (df2_updated["tokenization_type"] == "PAN"),
+        df1.filter(
+            (df1["attribute"] == "Consumer Account Number") & (df1["tokenization_type"] == "PAN")
+        ).select("value").first()[0]
+    ).otherwise(df2_updated["value"])
+)
 
 # Show the updated dataframe
 df2_final.show()
