1381c2b633d026729a1b527adc082e6f39950873
amit9p <amit_prasad@live.com>
2024-07-13 13:46:52 -0500

    Update Test2.py

--- Test2.py
+++ Test2.py
@@ -18,17 +18,18 @@ df2_ssn = df2.filter((col("attribute") == "Social security number") & (col("toke
 df2_can = df2.filter((col("attribute") == "Consumer Account Number") & (col("tokenization_type") == "PAN"))
 
 # Join df1 with filtered df2 dataframes
-df1_updated = df1 \
-    .join(df2_ssn.select("value").withColumnRenamed("value", "ssn_value"), on=[df1.attribute == "Social security number", df1.tokenization_type == "USTAXID"], how="left") \
-    .join(df2_can.select("value").withColumnRenamed("value", "can_value"), on=[df1.attribute == "Consumer Account Number", df1.tokenization_type == "PAN"], how="left")
+df1_joined = df1 \
+    .join(df2_ssn.select("value", "run_id", "segment", "row_position", "column_position", "file_type", "business_date").withColumnRenamed("value", "ssn_value"), on=[df1.attribute == "Social security number", df1.tokenization_type == "USTAXID"], how="left") \
+    .join(df2_can.select("value", "run_id", "segment", "row_position", "column_position", "file_type", "business_date").withColumnRenamed("value", "can_value"), on=[df1.attribute == "Consumer Account Number", df1.tokenization_type == "PAN"], how="left")
 
 # Update df1 value column based on the conditions
-df1_updated = df1_updated.withColumn(
+df1_updated = df1_joined.withColumn(
     "value",
     when((col("attribute") == "Social security number") & (col("tokenization_type") == "USTAXID"), col("ssn_value"))
     .when((col("attribute") == "Consumer Account Number") & (col("tokenization_type") == "PAN"), col("can_value"))
     .otherwise(col("value"))
-).select(col("account_id"), col("attribute"), col("value"), col("tokenization_type"))
+).select(col("df1.account_id"), col("df1.attribute"), col("value"), col("df1.tokenization_type"),
+         col("run_id"), col("segment"), col("row_position"), col("column_position"), col("file_type"), col("business_date"))
 
 # Show the updated dataframe
 df1_updated.show()
