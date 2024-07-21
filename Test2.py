15c405850837ce33ce201c331c9a94e17af36ab0
amit9p <amit_prasad@live.com>
2024-07-21 16:33:28 -0500

    Update Test2.py

--- Test2.py
+++ Test2.py
@@ -1,4 +1,24 @@
 
+
+# List of columns in the desired order (same as df2)
+columns_in_order = [
+    "business_date", "run_identifier", "output_file_type", "output_record_sequence", 
+    "output_field_sequence", "attribute", "formatted", "tokenization", 
+    "account_number", "segment"
+]
+
+# Reorder new_df to match the column order of df2
+new_df_reordered = new_df.select(columns_in_order)
+
+# Perform the union
+df3 = df2.union(new_df_reordered)
+
+# Show the new DataFrame
+df3.show()
+
+#####
+
+
 from pyspark.sql import SparkSession
 from pyspark.sql.functions import lit
 
