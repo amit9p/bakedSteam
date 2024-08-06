f3d166e443d904eaa5998a41c777c87d4bf7fd8e
amit9p <amit_prasad@live.com>
2024-08-07 01:02:46 +0530

    Update Test2.py

--- Test2.py
+++ Test2.py
@@ -1,22 +1,26 @@
 
+import boto3
 
-import configparser
+def test_after_writing_file_into_s3_metro2_str_incorrect():
+    # Setup
+    region_name = 'us-west-2'  # Example: use the appropriate region
+    s3 = boto3.client('s3', region_name=region_name)
+    
+    destination_bucket = 'your-bucket-name'  # Ensure this is unique or already exists
+    
+    try:
+        s3.create_bucket(Bucket=destination_bucket,
+                         CreateBucketConfiguration={'LocationConstraint': region_name})
+    except s3.exceptions.BucketAlreadyExists:
+        pass  # If the bucket already exists, we can pass
 
-def load_secrets(filename="secret.properties"):
-    config = configparser.ConfigParser()
-    config.read(filename)
-    return config['DEFAULT']['client_id'], config['DEFAULT']['client_secret']
-
-
-# Line 26 replacement
-client_id, client_secret = load_secrets()
-dev_creds = {
-    "client_id": client_id,
-    "client_secret": client_secret
-}
-
-
-
-[DEFAULT]
-client_id=your_client_id_here
-client_secret=your_client_secret_here
+    incorrect_value = "This is incorrect final output"
+    
+    # Function to write to S3
+    write_metro2_string_to_s3(destination_bucket, 'destination_folder', 'metro2_file_name', incorrect_value)
+    
+    # Function to read from S3
+    actual_result = read_file_from_s3(destination_bucket, f"destination_folder/metro2_file_name")
+    
+    # Assert
+    assert actual_result != metro2_str, "These should not be equal, something has gone wrong!"
