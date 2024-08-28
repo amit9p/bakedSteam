cc372ee9877d420ba4249f5634b3283a69e11030
amit9p <amit_prasad@live.com>
2024-08-28 13:21:45 -0500

    Update Test2.py

--- Test2.py
+++ Test2.py
@@ -1,10 +1,14 @@
--- Insert today's data into the target table from the source table, avoiding duplicates
-INSERT INTO target_table
-SELECT *
-FROM source_table
-WHERE date_column = CURRENT_DATE
-AND NOT EXISTS (
-    SELECT 1 
-    FROM target_table t
-    WHERE t.unique_id_column = source_table.unique_id_column
-);
+
+def setup_turing_config(dev_cred, tokenization, env):
+    env1 = "QA" if env in ["dev", "qa"] else "PROD"
+
+    npi_turing_config = {
+        "TURING_API_GATEWAY_NPI_URL": "https://api-turing-precede.cloud.capitalone.com" if env1 == "QA" else "https://api-turing-prcode.cloud.capitalone.com",
+        "TURING_API_OAUTH_URL": "https://api-precode.cloud.capitalone.com" if env1 == "QA" else "https://api.cloud.capitalone.com",
+        "TURING_API_NPI_SCOPE": f"tokenize:{tokenization}",
+        "TURING_OAUTH_CLIENT_ID": dev_cred["client_id"],
+        "TURING_OAUTH_CLIENT_SECRET": dev_cred["client_secret"],
+        "TURING_CLIENT_SSL_VERIFY": False,
+    }
+
+    return npi_turing_config
