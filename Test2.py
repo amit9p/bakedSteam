a4ec648c959fa57d1724d54484284b6b2b998572
amit9p <amit_prasad@live.com>
2024-09-17 13:25:19 -0500

    Update Test2.py

--- Test2.py
+++ Test2.py
@@ -1,13 +1,4 @@
 
-from pyspark.sql import functions as F
-from pyspark.sql.types import IntegerType, DateType
-
-# Assuming df is your DataFrame
-df = df.withColumn("business_date", F.to_date(F.col("business_date"))) \
-       .withColumn("run_identifier", F.col("run_identifier").cast(IntegerType()))
-
-# Now you can check the schema
-df.printSchema()
-
-# To see the changes applied
-df.show()
+--maxThreads 20
+--conf spark.executor.memory=10g
+--conf spark.sql.shuffle.partitions=120
