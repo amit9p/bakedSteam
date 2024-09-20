d7e66f20823bff4ad0c0c1c5f211b85f723c4582
amit9p <amit_prasad@live.com>
2024-09-20 13:19:26 -0500

    Update Test2.py

--- Test2.py
+++ Test2.py
@@ -13,3 +13,21 @@ Feature: Critical case for Glue job ETL process
     And if writing to S3 fails due to permissions or connection issues, raise a critical alert
     And I should see the error details logged in CloudWatch
     And the Jira X-Ray ticket should be created with a "Critical" severity level
+
+
+
+
+    Feature: Non-Critical case for Glue job ETL process
+
+  Background:
+    Given the Glue job "assembler_etl" is successfully deployed
+
+  Scenario: Non-critical warnings in Glue job ETL process
+    Given I have an input file in S3 at "<input_s3_path>"
+    When the Glue job "assembler_etl" runs
+    Then the job should read the input data from "<input_s3_path>"
+    And if there are missing fields in the input data, log a non-critical warning
+    And if the "detokenize" or "get_trade_lines" functions process partial data, log a non-critical warning
+    And if writing to S3 partially succeeds, log a non-critical warning
+    And I should see the warning details logged in CloudWatch
+    And the Jira X-Ray ticket should be created with a "Non-Critical" severity level
