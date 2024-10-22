bb86db99d85ca5627d78932933527044cf918ca9
amit9p <amit_prasad@live.com>
2024-10-22 15:24:16 -0500

    Update Test2.py

--- Test2.py
+++ Test2.py
@@ -1,13 +1,5 @@
 
-try:
-    response = glue_client.start_job_run(
-        JobName=glue_job,
-        Arguments={
-            '--input_s3_path': path,
-            '--env': environment,
-            '--output_s3_path': output_s3_path
-        }
-    )
-except Exception as e:
-    logger.exception("Error starting Glue job")
-    raise
+rows = pivot_df.select(concat_ws(field_separator, *pivot_df.columns).alias("metro2_string"))
+final_output_string = record_separator.join([row.metro2_string for row in rows.take(1000)])
+logger.debug(f"Final output string has {len(final_output_string)} characters")
+return final_output_string
