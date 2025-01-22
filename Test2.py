e6b79d02db7062912743c128ee1d9ff602695663
amit9p <amit_prasad@live.com>
2025-01-22 15:36:35 -0600

    Update Test2.py

--- Test2.py
+++ Test2.py
@@ -1,16 +1,46 @@
 
-import pytest
-from pyspark.sql.types import StructType, StructField, StringType, IntegerType
-
-def test_generic_exception():
-    # Pass a completely invalid object instead of a DataFrame to trigger a generic exception
-    invalid_input = "This is not a DataFrame"
-
-    # Use pytest to assert that a generic exception is raised
-    with pytest.raises(Exception) as exc_info:
-        calculate_current_balance(invalid_input)  # This should raise a generic exception
-
-    # Assert the exception is an AttributeError
-    assert isinstance(exc_info.value, AttributeError)
-    assert "withColumn" in str(exc_info.value)
-    print("Generic exception test case passed!")
+SELECT 
+    *,
+    RANK() OVER (
+        PARTITION BY PMT_PLAN_DETL_ID, 
+                     SLTN_ENRLNT_ID, 
+                     EVT_TYPE_TXT, 
+                     PMT_STAT_TXT, 
+                     PMT_UPDTD_TS, 
+                     PMT_LAST_STAT_TS, 
+                     LEGL_AFIL_CD, 
+                     PMT_CAT_TXT, 
+                     PMT_CRETD_BY_ID, 
+                     BUS_DT
+        ORDER BY COAF_PUBLN_ID ASC
+    ) AS RN
+FROM (
+    SELECT 
+        COAF_DB.COAF_TB.DELQ_ACCT_SLTN_PMT_PLAN_FACT_SEQ.NEXTVAL AS DELQ_ACCT_SLTN_PMT_PLAN_FACT_ID,
+        ACCT_DIM_ID,
+        LOAN_ACCT_NUM,
+        SLTN_ENRLNT_ID,
+        PMT_PLAN_DETL_ID,
+        SRC_PRIM_KEY,
+        SOR_ID,
+        LEGL_AFIL_CD,
+        COAF_PUBLN_ID,
+        BUS_DT,
+        BUS_TS,
+        ELT_AUDT_TAG,
+        LOAN_ACCT_ID,
+        PMT_ID,
+        OLD_PMT_ID,
+        EVT_TYPE_TXT,
+        PMT_AMT,
+        PMT_RECVD_AMT,
+        PMT_CAT_TXT,
+        PMT_STAT_TXT,
+        PMT_REVERSE_REASN_TXT,
+        PMT_EFFT_DT,
+        PMT_EDITD_TS,
+        PMT_UPDTD_TS,
+        PMT_CRETD_TS,
+        PMT_LAST_STAT_TS
+    FROM COAF_DB.COAF_TB.DELQ_ACCT_SLTN_PMT_PLAN_FACT
+) AS SOURCE_TABLE;
