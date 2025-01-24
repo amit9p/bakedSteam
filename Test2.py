45ebf9003a011c811d0406a6d82d2cc3e050ca02
amit9p <amit_prasad@live.com>
2025-01-24 17:32:59 -0600

    Update Test2.py

--- Test2.py
+++ Test2.py
@@ -1,4 +1,15 @@
 
+DELETE FROM your_table
+WHERE id NOT IN (
+    SELECT MIN(id)
+    FROM your_table
+    GROUP BY column1, column2, column3 -- Columns to identify duplicates
+);
+
+
+
+
+
 DELETE FROM coaf_db.collab.lab_svcg.DELQ_ACCT_SLTN_PMT_PLAN_FACT
 WHERE SRC_PRIM_KEY IN (
     SELECT SRC_PRIM_KEY
