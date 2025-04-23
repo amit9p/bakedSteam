f974acc834c9839d5762d95d7116b13f1dee3354
amit9p <amit_prasad@live.com>
2025-04-23 13:20:02 +0530

    Update Test2.py

--- Test2.py
+++ Test2.py
@@ -1,17 +1,18 @@
 
-def extract_primary_keys_by_field(df, field_list):
-    result = {}
-    for field in field_list:
-        keys = (df.filter(df.field_name == field)
-                  .select("primary_key")
-                  .distinct()
-                  .rdd.flatMap(lambda x: x)
-                  .collect())
-        result[field] = keys
-    return result
+def read_with_credentials(spark, access_key, secret_key, session_token, s3_path):
+    hadoop_conf = spark._jsc.hadoopConfiguration()
+    hadoop_conf.set("fs.s3a.access.key", access_key)
+    hadoop_conf.set("fs.s3a.secret.key", secret_key)
+    hadoop_conf.set("fs.s3a.session.token", session_token)
+    return spark.read.parquet(s3_path)
 
+# Example
+spark = SparkSession.builder.appName("SingleSpark").getOrCreate()
 
+df1 = read_with_credentials(spark, access_key1, secret_key1, session_token1, "s3a://bucket1/path")
+df2 = read_with_credentials(spark, access_key2, secret_key2, session_token2, "s3a://bucket2/path")
 
-fields = ["scheduled_payment_amount", "consumer_account_number"]
-output = extract_primary_keys_by_field(your_dataframe, fields)
-print(output)
+df1.show()
+df2.show()
+
+spark.stop()
