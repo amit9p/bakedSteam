1975566c8cd31df1346658f777e5460de4a0cf50
amit9p <amit_prasad@live.com>
2025-05-07 13:51:55 +0530

    Update Test2.py

--- Test2.py
+++ Test2.py
@@ -1,4 +1,9 @@
 
+check_if_any_are_null([col(BaseSegment.portfolio_type.str)], lit(None))
+
+
+
+
 joined_df = portfolio_type_df.join(
     account_df,
     on=portfolio_type_df[BaseSegment.account_id.str] == account_df[CCAccount.account_id.str],
