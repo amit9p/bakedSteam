dec7e9021d0a124d762457a786f4289041c2a8cf
amit9p <amit_prasad@live.com>
2025-05-15 03:38:40 +0530

    Update Test2.py

--- Test2.py
+++ Test2.py
@@ -1,16 +1,17 @@
 
+
 from pyspark.sql import DataFrame
-from pyspark.sql.functions import col
+from pyspark.sql.functions import col, round
 from ecbr_card_self_service.schemas.sbfe.ab_segment import ABSegment
 
 def get_current_credit_limit(ecbr_df: DataFrame) -> DataFrame:
     """
-    Returns a DataFrame with account_id and current_credit_limit fields,
-    where current_credit_limit is derived from ecbr_df.spending_limit.
+    Returns account_id and current_credit_limit (rounded to whole dollar)
+    from the spending_limit field in ecbr_df.
     """
     result_df = ecbr_df.withColumn(
         ABSegment.current_credit_limit.str,
-        col("spending_limit")
+        round(col("spending_limit"))
     )
 
     return result_df.select(
