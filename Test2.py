4576e8db01b5382a5d9a41de84f9c3096b8222d1
amit9p <amit_prasad@live.com>
2025-05-16 01:11:59 +0530

    Update Test2.py

--- Test2.py
+++ Test2.py
@@ -1,4 +1,63 @@
 
+def test_get_current_credit_limit(spark: SparkSession):
+    input_df = create_partially_filled_dataset(
+        spark,
+        CCAccount,
+        data=[
+            {"account_id": "A1", "available_spending_amount": 501},
+            {"account_id": "A2", "available_spending_amount": None}
+        ]
+    )
+
+    expected_df = create_partially_filled_dataset(
+        spark,
+        ABSegment,
+        data=[
+            {"account_id": "A1", "current_credit_limit": 501},
+            {"account_id": "A2", "current_credit_limit": -999999999}
+        ]
+    ).select(ABSegment.account_id, ABSegment.current_credit_limit)
+
+    result_df = get_current_credit_limit(input_df)
+
+    assert_df_equality(result_df, expected_df, ignore_row_order=True, ignore_nullable=True)
+
+
+
+def test_get_original_credit_limit(spark: SparkSession):
+    input_df = create_partially_filled_dataset(
+        spark,
+        CCAccount,
+        data=[
+            {"account_id": "A1", "available_spending_amount": 999},
+            {"account_id": "A2", "available_spending_amount": None}
+        ]
+    )
+
+    expected_df = create_partially_filled_dataset(
+        spark,
+        ABSegment,
+        data=[
+            {"account_id": "A1", "original_credit_limit": 999},
+            {"account_id": "A2", "original_credit_limit": -999999999}
+        ]
+    ).select(ABSegment.account_id, ABSegment.original_credit_limit)
+
+    result_df = get_original_credit_limit(input_df)
+
+    assert_df_equality(result_df, expected_df, ignore_row_order=True, ignore_nullable=True)
+
+
+############
+
+
+
+
+
+
+
+
+
 
 def get_current_credit_limit(input_df: DataFrame) -> DataFrame:
     """
