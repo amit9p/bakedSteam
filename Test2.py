99ac9b3bf531cb34184168340a3f98bd149225a5
amit9p <amit_prasad@live.com>
2025-05-15 10:30:28 +0530

    Update Test2.py

--- Test2.py
+++ Test2.py
@@ -1,4 +1,27 @@
 
+from pyspark.sql import DataFrame
+from pyspark.sql.functions import col
+from ecbr_card_self_service.schemas.sbfe.ab_segment import ABSegment
+
+def get_original_credit_limit(input_df: DataFrame) -> DataFrame:
+    """
+    Returns account_id and original_credit_limit (same as current_credit_limit),
+    rounded to whole dollar as per AB Field 20 definition.
+    """
+    result_df = input_df.withColumn(
+        ABSegment.original_credit_limit.str,
+        col(ABSegment.current_credit_limit.str)  # Already rounded in previous method
+    )
+
+    return result_df.select(
+        ABSegment.account_id,
+        ABSegment.original_credit_limit
+    )
+
+
+
+
+
 
 from pyspark.sql import DataFrame
 from pyspark.sql.functions import col, round
