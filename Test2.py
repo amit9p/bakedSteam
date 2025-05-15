aa8e762a5e4295dc3aee3295d23d93896df19b2d
amit9p <amit_prasad@live.com>
2025-05-16 02:07:03 +0530

    Update Test2.py

--- Test2.py
+++ Test2.py
@@ -1,21 +1,49 @@
 
-def test_get_current_credit_limit():
-    input_df = create_partially_filled_dataset(
+
+def test_get_current_credit_limit(spark: SparkSession):
+    ccaccount_data = create_partially_filled_dataset(
+        spark,
         CCAccount,
         data=[
-            {"account_id": "A1", "available_spending_amount": 501},
-            {"account_id": "A2", "available_spending_amount": None},
-        ]
+            {CCAccount.account_id: "A1", CCAccount.available_spending_amount: 501},
+            {CCAccount.account_id: "A2", CCAccount.available_spending_amount: None},
+        ],
     )
 
-    expected_df = create_partially_filled_dataset(
+    result_df = get_current_credit_limit(ccaccount_data)
+
+    expected_data = create_partially_filled_dataset(
+        spark,
         ABSegment,
         data=[
-            {"account_id": "A1", "current_credit_limit": 501},
-            {"account_id": "A2", "current_credit_limit": -999999999},
-        ]
+            {ABSegment.account_id: "A1", ABSegment.current_credit_limit: 501},
+            {ABSegment.account_id: "A2", ABSegment.current_credit_limit: -999999999},
+        ],
     ).select(ABSegment.account_id, ABSegment.current_credit_limit)
 
-    result_df = get_current_credit_limit(input_df)
+    assert_df_equality(result_df, expected_data, ignore_row_order=True, ignore_nullable=True)
 
-    assert_df_equality(result_df, expected_df, ignore_row_order=True, ignore_nullable=True)
+
+
+def test_get_original_credit_limit(spark: SparkSession):
+    ccaccount_data = create_partially_filled_dataset(
+        spark,
+        CCAccount,
+        data=[
+            {CCAccount.account_id: "A1", CCAccount.available_spending_amount: 999},
+            {CCAccount.account_id: "A2", CCAccount.available_spending_amount: None},
+        ],
+    )
+
+    result_df = get_original_credit_limit(ccaccount_data)
+
+    expected_data = create_partially_filled_dataset(
+        spark,
+        ABSegment,
+        data=[
+            {ABSegment.account_id: "A1", ABSegment.original_credit_limit: 999},
+            {ABSegment.account_id: "A2", ABSegment.original_credit_limit: -999999999},
+        ],
+    ).select(ABSegment.account_id, ABSegment.original_credit_limit)
+
+    assert_df_equality(result_df, expected_data, ignore_row_order=True, ignore_nullable=True)
