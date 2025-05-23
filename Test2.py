e89554bb00d33b0de222ed76ff11cfea13e9cb90
amit9p <amit_prasad@live.com>
2025-05-23 20:34:52 +0530

    Update Test2.py

--- Test2.py
+++ Test2.py
@@ -1,75 +1,41 @@
 
-Metro2 expects fixed-width flat files
-
-If you write "0" instead of "000000000000", it will:
-
-Misalign the field
-
-Shift other fields
-
-Break the file structure
-
-Fail validation by credit bureaus
-
-
-
-
-from pyspark.sql.types import StringType
-from typespark import Column, Schema
-
-class ABSegment(Schema):
-    ...
-    payment_amount_scheduled: Column[StringType]  # <-- Add this line
-
-
-
-constant_zero_payment = "000000000000"
-
-
-def get_payment_amount_scheduled(ccaccount_df: DataFrame) -> DataFrame:
+def get_balloon_payment_due_date(ccaccount_df: DataFrame) -> DataFrame:
     """
-    :param ccaccount_df: Input DataFrame with account_id
-    :return: Output DataFrame with account_id and payment_amount_scheduled (hardcoded 12-digit zero string)
+    :param ccaccount_df: Input DataFrame
+    :return: Output DataFrame with account_id and balloon_payment_due_date set to null
     """
     result_df = ccaccount_df.withColumn(
-        ABSegment.payment_amount_scheduled.str,
-        lit(constant_zero_payment)
+        ABSegment.balloon_payment_due_date.str,
+        lit(None).cast(DateType())
     )
 
     return result_df.select(
         ABSegment.account_id,
-        ABSegment.payment_amount_scheduled
+        ABSegment.balloon_payment_due_date
     )
 
-def test_get_payment_amount_scheduled(spark: SparkSession):
+
+
+def test_get_balloon_payment_due_date(spark: SparkSession):
     ccaccount_data = create_partially_filled_dataset(
         spark,
         CCAccount,
-        [
+        data=[
             {CCAccount.account_id: "1"},
-            {CCAccount.account_id: "004004004"},
+            {CCAccount.account_id: "X004"}
         ],
     )
 
-    result_df = get_payment_amount_scheduled(ccaccount_data)
+    result_df = get_balloon_payment_due_date(ccaccount_data)
 
     expected_data = create_partially_filled_dataset(
         spark,
         ABSegment,
-        [
-            {
-                ABSegment.account_id: "1",
-                ABSegment.payment_amount_scheduled: constant_zero_payment,
-            },
-            {
-                ABSegment.account_id: "004004004",
-                ABSegment.payment_amount_scheduled: constant_zero_payment,
-            },
+        data=[
+            {ABSegment.account_id: "1", ABSegment.balloon_payment_due_date: None},
+            {ABSegment.account_id: "X004", ABSegment.balloon_payment_due_date: None},
         ],
-    ).select(
-        ABSegment.account_id,
-        ABSegment.payment_amount_scheduled
-    )
+    ).select(ABSegment.account_id, ABSegment.balloon_payment_due_date)
 
     assert_df_equality(
         result_df,
