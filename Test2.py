2670f1feb3d9ed4812724cb80c520c561411c389
amit9p <amit_prasad@live.com>
2025-06-19 17:56:56 -0500

    Update Test2.py

--- Test2.py
+++ Test2.py
@@ -1,5 +1,73 @@
 
 from datetime import datetime
+from chispa import assert_df_equality
+from typespark import create_partially_filled_dataset
+from ecbr_card_self_service.ecbr_calculations.constants import DEFAULT_ERROR_DATE
+from ecbr_card_self_service.ecbr_calculations.fields.base.date_closed import date_closed
+from ecbr_card_self_service.schemas.base_segment import BaseSegment
+from ecbr_card_self_service.schemas.cc_account import CCAccount
+
+def test_date_closed(spark):
+    # Input dataset
+    data = create_partially_filled_dataset(
+        spark,
+        CCAccount,
+        data=[
+            {
+                CCAccount.account_id: "1",
+                CCAccount.account_close_date: datetime(2024, 12, 9).date(),
+                CCAccount.charge_off_date: datetime(2024, 12, 10).date(),
+            },
+            {
+                CCAccount.account_id: "2",
+                CCAccount.account_close_date: None,
+                CCAccount.charge_off_date: datetime(2024, 12, 8).date(),
+            },
+            {
+                CCAccount.account_id: "3",
+                CCAccount.account_close_date: None,
+                CCAccount.charge_off_date: datetime(2024, 12, 7).date(),
+            },
+            {
+                CCAccount.account_id: "4",
+                CCAccount.account_close_date: None,
+                CCAccount.charge_off_date: None,
+            },
+        ]
+    )
+
+    # Expected output
+    expected = create_partially_filled_dataset(
+        spark,
+        BaseSegment,
+        data=[
+            {
+                BaseSegment.account_id: "1",
+                BaseSegment.date_closed: datetime(2024, 12, 9).date(),
+            },
+            {
+                BaseSegment.account_id: "2",
+                BaseSegment.date_closed: datetime(2024, 12, 8).date(),
+            },
+            {
+                BaseSegment.account_id: "3",
+                BaseSegment.date_closed: datetime(2024, 12, 7).date(),
+            },
+            {
+                BaseSegment.account_id: "4",
+                BaseSegment.date_closed: DEFAULT_ERROR_DATE,
+            },
+        ]
+    )
+
+    # Transformation
+    result_df = date_closed(data)
+
+    # Assertion
+    assert_df_equality(result_df, expected, ignore_row_order=True)
+
+******
+from datetime import datetime
 from ecbr_card_self_service.ecbr_calculations.fields.date_closed import date_closed
 from ecbr_card_self_service.schemas.base_segment import BaseSegment
 from ecbr_card_self_service.schemas.cc_account import CCAccount
