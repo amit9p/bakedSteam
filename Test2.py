5fb6ae9f1c99eaaba1d5f0464df7add123ba516a
amit9p <amit_prasad@live.com>
2025-06-20 09:35:50 -0500

    Update Test2.py

--- Test2.py
+++ Test2.py
@@ -3,38 +3,33 @@ from datetime import datetime
 from chispa import assert_df_equality
 from pyspark.sql import SparkSession
 from typedspark import create_partially_filled_dataset
-
 from ecbr_card_self_service.ecbr_calculations.fields.base.date_closed import date_closed
 from ecbr_card_self_service.schemas.base_segment import BaseSegment
 from ecbr_card_self_service.schemas.cc_account import CCAccount
 from ecbr_card_self_service.ecbr_calculations.constants import DEFAULT_ERROR_DATE
 
 def test_date_closed(spark: SparkSession):
-    # Input data covering all scenarios
+    # Input test data
     data = create_partially_filled_dataset(
         spark,
         CCAccount,
         data=[
             {
-                # Valid account_close_date, valid charge_off_date (should use account_close_date)
                 CCAccount.account_id: "1",
                 CCAccount.account_close_date: datetime(year=2024, month=12, day=9).date(),
                 CCAccount.charge_off_date: datetime(year=2024, month=12, day=10).date(),
             },
             {
-                # Null account_close_date, valid charge_off_date (should use charge_off_date)
                 CCAccount.account_id: "2",
                 CCAccount.account_close_date: None,
                 CCAccount.charge_off_date: datetime(year=2024, month=12, day=8).date(),
             },
             {
-                # Blank account_close_date, null charge_off_date (should use DEFAULT_ERROR_DATE)
                 CCAccount.account_id: "3",
-                CCAccount.account_close_date: "",
+                CCAccount.account_close_date: None,  # Blank handled in logic
                 CCAccount.charge_off_date: None,
             },
             {
-                # Both null (should use DEFAULT_ERROR_DATE)
                 CCAccount.account_id: "4",
                 CCAccount.account_close_date: None,
                 CCAccount.charge_off_date: None,
@@ -42,7 +37,6 @@ def test_date_closed(spark: SparkSession):
         ]
     )
 
-    # Run transformation
     result_df = date_closed(data)
 
     # Expected output
@@ -69,5 +63,4 @@ def test_date_closed(spark: SparkSession):
         ]
     ).select(BaseSegment.account_id, BaseSegment.date_closed)
 
-    # Assertion
     assert_df_equality(result_df, expected_data, ignore_row_order=True)
