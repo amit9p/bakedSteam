9526aa38d05f74d884c2fb2aac1b41848cd350ee
amit9p <amit_prasad@live.com>
2025-06-19 23:34:57 -0500

    Update Test2.py

--- Test2.py
+++ Test2.py
@@ -1,5 +1,74 @@
 
 
+from datetime import datetime
+from chispa import assert_df_equality
+from pyspark.sql import SparkSession
+from typedspark import create_partially_filled_dataset
+from ecbr_card_self_service.ecbr_calculations.fields.base.date_closed import date_closed
+from ecbr_card_self_service.schemas.base_segment import BaseSegment
+from ecbr_card_self_service.schemas.cc_account import CCAccount
+
+def test_date_closed(spark: SparkSession):
+    default_date_format = "%Y-%m-%d"
+    
+    # Input
+    data = create_partially_filled_dataset(
+        spark,
+        CCAccount,
+        data=[
+            {
+                CCAccount.account_id: "1",
+                CCAccount.account_close_date: datetime(2024, 12, 9).date(),
+                CCAccount.charge_off_date: datetime.strptime("2024-12-10", default_date_format).date(),
+            },
+            {
+                CCAccount.account_id: "2",
+                CCAccount.account_close_date: None,
+                CCAccount.charge_off_date: datetime.strptime("2024-12-08", default_date_format).date(),
+            },
+            {
+                CCAccount.account_id: "3",
+                CCAccount.account_close_date: None,
+                CCAccount.charge_off_date: datetime.strptime("2024-12-07", default_date_format).date(),
+            },
+            {
+                CCAccount.account_id: "4",
+                CCAccount.account_close_date: None,
+                CCAccount.charge_off_date: None,
+            },
+        ],
+    )
+
+    result_df = date_closed(data)
+
+    expected_data = create_partially_filled_dataset(
+        spark,
+        BaseSegment,
+        data=[
+            {
+                BaseSegment.account_id: "1",
+                BaseSegment.date_closed: datetime(2024, 12, 9).date(),
+            },
+            {
+                BaseSegment.account_id: "2",
+                BaseSegment.date_closed: datetime(2024, 12, 8).date(),
+            },
+            {
+                BaseSegment.account_id: "3",
+                BaseSegment.date_closed: datetime(2024, 12, 7).date(),
+            },
+            {
+                BaseSegment.account_id: "4",
+                BaseSegment.date_closed: datetime.strptime("1900-01-01", default_date_format).date(),
+            },
+        ],
+    )
+
+    assert_df_equality(result_df, expected_data, ignore_row_order=True)
+
+
+____
+
 
 from pyspark.sql import DataFrame
 from pyspark.sql.functions import when, trim, col, lit
