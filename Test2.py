cefda2cc898ce9bd75c00c5b404f754f26fc5b2c
amit9p <amit_prasad@live.com>
2025-06-23 13:48:50 -0500

    Update Test2.py

--- Test2.py
+++ Test2.py
@@ -1,44 +1,24 @@
 
-.when(
-    CustomerInformation.bankruptcy_first_filed_date.isNotNull() &
-    CCAccount.date_of_first_delinquency.isNull() &
-    CCAccount.account_open_date.isNull(),
-    lit(DEFAULT_ERROR_DATE)
+
+bankruptcy_file_date_is_null_or_blank = (
+    CustomerInformation.bankruptcy_first_filed_date.isNull() |
+    (trim(CustomerInformation.bankruptcy_first_filed_date) == "")
+)
+
+start_date_of_delinquency_is_null_or_blank = (
+    CCAccount.date_of_first_delinquency.isNull() |
+    (trim(CCAccount.date_of_first_delinquency) == "")
+)
+
+account_open_date_is_null_or_blank = (
+    CCAccount.account_open_date.isNull() |
+    (trim(CCAccount.account_open_date) == "")
 )
 
 
-
-BaseSegment.date_of_first_delinquency: [
-    None,  # id 1
-    datetime(2024, 1, 13).date(),  # id 2
-    datetime(2024, 1, 13).date(),  # id 3
-    None,  # id 4
-    datetime(2024, 1, 12).date(),  # id 5
-    datetime(2024, 1, 12).date(),  # id 6
-    datetime(2024, 1, 13).date(),  # id 7
-    datetime(2024, 1, 13).date(),  # id 8
-    datetime(2024, 1, 13).date(),  # id 9
-    DEFAULT_ERROR_DATE,  # ✅ id 10
-]
-
-
-_____
-expected_data = create_partially_filled_dataset(
-    spark,
-    BaseSegment,
-    data={
-        BaseSegment.account_id: ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10"],
-        BaseSegment.date_of_first_delinquency: [
-            None,  # id 1: still None
-            datetime(year=2024, month=1, day=13).date(),  # id 2
-            datetime(year=2024, month=1, day=13).date(),  # id 3
-            None,  # id 4
-            datetime(year=2024, month=1, day=12).date(),  # id 5
-            datetime(year=2024, month=1, day=12).date(),  # id 6
-            datetime(year=2024, month=1, day=13).date(),  # id 7
-            datetime(year=2024, month=1, day=13).date(),  # id 8
-            datetime(year=2024, month=1, day=13).date(),  # id 9
-            DEFAULT_ERROR_DATE,  # id 10: invalid input scenario
-        ],
-    }
+.when(
+    bankruptcy_file_date_is_null_or_blank &
+    start_date_of_delinquency_is_null_or_blank &
+    account_open_date_is_null_or_blank,
+    lit(DEFAULT_ERROR_DATE)
 )
