9c4464d15157d0caca59d443df3112a0e415944a
amit9p <amit_prasad@live.com>
2025-06-23 12:39:43 -0500

    Update Test2.py

--- Test2.py
+++ Test2.py
@@ -1,4 +1,19 @@
 
+BaseSegment.date_of_first_delinquency: [
+    None,  # id 1
+    datetime(2024, 1, 13).date(),  # id 2
+    datetime(2024, 1, 13).date(),  # id 3
+    None,  # id 4
+    datetime(2024, 1, 12).date(),  # id 5
+    datetime(2024, 1, 12).date(),  # id 6
+    datetime(2024, 1, 13).date(),  # id 7
+    datetime(2024, 1, 13).date(),  # id 8
+    datetime(2024, 1, 13).date(),  # id 9
+    DEFAULT_ERROR_DATE,  # ✅ id 10
+]
+
+
+_____
 expected_data = create_partially_filled_dataset(
     spark,
     BaseSegment,
