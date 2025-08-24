4fc2286ab86c48fc90c69eae519397322e690073
amit9p <amit_prasad@live.com>
2025-08-24 15:08:03 -0500

    Update Test2.py

--- Test2.py
+++ Test2.py
@@ -1,4 +1,28 @@
 
+{
+  "Input Attribute": "identification_number",
+  "Sub Fields": [
+    {
+      "Name": "equifax",
+      "Proposed Data Product": "Primary Key (All)",
+      "Attribute Status": "Modeling Complete"
+    },
+    {
+      "Name": "experian",
+      "Proposed Data Product": "Primary Key (All)",
+      "Attribute Status": "Modeling Complete"
+    },
+    {
+      "Name": "transunion",
+      "Proposed Data Product": "Primary Key (All)",
+      "Attribute Status": "Modeling Complete"
+    }
+  ],
+  "Standard Field Name": "account_id",
+  "DP Confirmed": "Y",
+  "Finalized": "N"
+}
+
 import CsvTable from '@site/src/utilities/csvTable';
 
 # Calculator O/P Schema
