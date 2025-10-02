d9d14b045296ba6210e5e194b5b46ca12cedcc31
amit9p <amit_prasad@live.com>
2025-10-02 11:17:03 -0500

    Update Test2.py

--- Test2.py
+++ Test2.py
@@ -1,16 +1,19 @@
 
 from pyspark.sql.functions import col
-from pyspark.sql.types import LongType, StringType, DateType
+from pyspark.sql.types import LongType, StringType, TimestampType, DateType
 
 df_converted = (
     df
-    .withColumn("customer_pk_id", col("customer_pk_id").cast(LongType()))
-    .withColumn("customer_id", col("customer_id").cast(LongType()))
+    .withColumn("override_id", col("override_id").cast(LongType()))
     .withColumn("account_id", col("account_id").cast(LongType()))
-    .withColumn("consumer_info_indicator", col("consumer_info_indicator").cast(StringType()))
-    .withColumn("ownership_type_ecoa", col("ownership_type_ecoa").cast(StringType()))
     .withColumn("reporting_status", col("reporting_status").cast(StringType()))
     .withColumn("reporting_reason", col("reporting_reason").cast(StringType()))
+    .withColumn("initiated_date", col("initiated_date").cast(TimestampType()))
+    .withColumn("initiated_by", col("initiated_by").cast(StringType()))
+    .withColumn("closed_date", col("closed_date").cast(TimestampType()))
+    .withColumn("closed_by", col("closed_by").cast(StringType()))
+    .withColumn("source", col("source").cast(StringType()))
+    .withColumn("source_uri", col("source_uri").cast(StringType()))
     .withColumn("snap_dt", col("snap_dt").cast(DateType()))
     .withColumn("edw_publn_id", col("edw_publn_id").cast(LongType()))
     .withColumn("instnc_id", col("instnc_id").cast(StringType()))
