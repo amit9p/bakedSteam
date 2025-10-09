fff3d6292b373e6f536cdf1ecf2cc615193ba51a
amit9p <amit_prasad@live.com>
2025-10-09 10:06:04 -0500

    Update Test2.py

--- Test2.py
+++ Test2.py
@@ -1,41 +1,11 @@
 
-from pyspark.sql.functions import lit
+Hi team, our Glue job failed with:
+“The specified subnet does not have enough free IP addresses to satisfy the request. Please provide a connection with a subnet which is available.”
 
-df_updated = df.withColumn("identification_number", lit("CAPDISC"))
+Job: ebcrp-dfs11t-latest-load-onelake-job-vo1x1t02
+Run ID: jr_cff83331ce14352db4d8d1551e461da112ab9445e191e520acd22361bff47464
+Worker type/count: G.4X / 138
+Triggered via Step Function.
 
-unique_account_ids = df.select("account_id").distinct()
-
-df_renamed = (
-    df
-    .withColumnRenamed("run_id", "ecbr_run_id")
-    .withColumnRenamed("run_id_utc_timestamp", "ecbr_run_id_utc_timestamp")
-    .withColumnRenamed("run_id_date", "ecbr_run_id_date")
-)
-
-
-
-from pyspark.sql.functions import lit
-
-# Replace all values in 'identification_number' column with 'CAPDISK'
-df_updated = df.withColumn("identification_number", lit("CAPDISK"))
-
-
-from pyspark.sql.functions import col
-from pyspark.sql.types import LongType, StringType, TimestampType, DateType
-
-df_converted = (
-    df
-    .withColumn("override_id", col("override_id").cast(LongType()))
-    .withColumn("account_id", col("account_id").cast(LongType()))
-    .withColumn("reporting_status", col("reporting_status").cast(StringType()))
-    .withColumn("reporting_reason", col("reporting_reason").cast(StringType()))
-    .withColumn("initiated_date", col("initiated_date").cast(TimestampType()))
-    .withColumn("initiated_by", col("initiated_by").cast(StringType()))
-    .withColumn("closed_date", col("closed_date").cast(TimestampType()))
-    .withColumn("closed_by", col("closed_by").cast(StringType()))
-    .withColumn("source", col("source").cast(StringType()))
-    .withColumn("source_uri", col("source_uri").cast(StringType()))
-    .withColumn("snap_dt", col("snap_dt").cast(DateType()))
-    .withColumn("edw_publn_id", col("edw_publn_id").cast(LongType()))
-    .withColumn("instnc_id", col("instnc_id").cast(StringType()))
-)
+Looks like subnet exhaustion in the Glue connection for our tenant.
+Could you please check IP availability or add another subnet to the connection?
