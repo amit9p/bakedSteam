eb06c55220ddb36d24b4b9f113a7642d337aa164
amit9p <amit_prasad@live.com>
2025-11-07 23:32:58 -0600

    Update Test2.py

--- Test2.py
+++ Test2.py
@@ -1,20 +1,17 @@
+datasets:
+  1:
+    name: account_service_account_os
+    s3_path: s3://your-bucket/path/to/account_service_account_os/
+    catalog_id: 7d2981d5-4a5a-49f7-a937-8f18f5ad0a44
 
+  2:
+    name: credit_bureau_reporting_card_cl
+    s3_path: s3://your-bucket/path/to/credit_bureau_reporting_card_cl/
+    catalog_id: 45d27c2e-3b44-4b08-9a2d-b9500a092d68
 
-{
-  "env": "dev",
-  "log_level": "INFO",
+  3:
+    name: loan_application_ls
+    s3_path: s3://your-bucket/path/to/loan_application_ls/
+    catalog_id: 7327626d-8d5c-450a-9c84-14250d00b29d
 
-  "datasets": {
-    "1": {
-      "name": "account_service_account_os",
-      "s3_path": "s3://bucket/path/account_service_account_os/",
-      "catalog_id": "7d2981d5-4a5a-49f7-a937-8f18f5ad0a44"
-    },
-    "2": {
-      "name": "credit_bureau_reporting_card_cl",
-      "s3_path": "s3://bucket/path/credit_bureau_reporting_card_cl/",
-      "catalog_id": "45d27c2e-3b44-4b08-9a2d-b9500a092d68"
-    }
-    // add the rest...
-  }
-}
+  # … add all 11 datasets here
