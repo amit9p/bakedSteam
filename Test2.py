d76113dbb5528be46f7cf532bdc748f3025a2788
amit9p <amit_prasad@live.com>
2025-11-20 16:47:18 -0600

    Update Test2.py

--- Test2.py
+++ Test2.py
@@ -1,3 +1,4 @@
+This dataset contains credit information for secondary account holders from Discover charged-off accounts. The data originates from Omega and includes Enterprise Credit Bureau Reporting Consolidator output J2 segment elements, which serve as inputs to the Enterprise Credit Bureau Reporting Calculator. The primary users of this dataset are Credit Bureau Reporting data analysts, and it supports processing within the Enterprise Credit Bureau Reporting Calculator component.
 
 
 
