98f8aa159fc9dc195bdca6fee65ad94b636de372
amit9p <amit_prasad@live.com>
2025-12-30 22:42:11 +0530

    Update Test2.py

--- Test2.py
+++ Test2.py
@@ -1,4 +1,22 @@
+expected_df = create_partially_filled_dataset(
+    spark,
+    ECBRGeneratedFields,
+    data=[
+        {
+            ECBRGeneratedFields.account_id: 21,
+            ECBRGeneratedFields.is_identity_fraud_claimed_on_account: True,
+        },
+        {
+            ECBRGeneratedFields.account_id: 22,
+            ECBRGeneratedFields.is_identity_fraud_claimed_on_account: False,
+        },
+    ],
+).select(
+    ECBRGeneratedFields.account_id.str,
+    ECBRGeneratedFields.is_identity_fraud_claimed_on_account.str,
+)
 
+-----
 fraud_flag_df = fraud_investigation_notification(incidents_df) \
     .select(
         BaseSegment.account_id_str,
