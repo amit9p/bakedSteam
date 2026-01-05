749695f7d20548e6f48cae7cadac67b2e623bf86
amit9p <amit_prasad@live.com>
2026-01-05 23:37:17 +0530

    Update Test2.py

--- Test2.py
+++ Test2.py
@@ -2,3 +2,19 @@
 
 mock_detokenize.assert_not_called()
 mock_validate_detokenized_df.assert_not_called()
+
+
+
+mock_get_args_for_job.assert_called_once()
+mock_reinitialize_logger.assert_called_once()
+
+mock_read_parquet.assert_called_once()
+mock_create_s3_client.assert_called_once()
+
+mock_detokenize.assert_not_called()
+mock_validate_detokenized_df.assert_not_called()
+
+mock_get_trade_lines.assert_called_once()
+
+# If you returned only one bureau
+self.assertEqual(mock_write_to_efg.call_count, 1)
