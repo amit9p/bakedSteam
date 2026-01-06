538fe2ec58037313736c261cc3f7d0a716443754
amit9p <amit_prasad@live.com>
2026-01-06 10:38:09 +0530

    Update Test2.py

--- Test2.py
+++ Test2.py
@@ -1,5 +1,5 @@
 
-
+common.logger.info.assert_called_with("Detokenization validation passed: No ERROR strings found in formatted column")
 @patch("utils.common.upper")
 @patch("utils.common.col")
 def test_validate_detokenized_dataframe_with_errors(self, mock_col, mock_upper):
