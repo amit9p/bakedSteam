44704a023d7050d421b3e56e7e1ba78ee0e35fc5
amit9p <amit_prasad@live.com>
2026-01-06 10:39:30 +0530

    Update Test2.py

--- Test2.py
+++ Test2.py
@@ -1,4 +1,10 @@
 
+common.logger.info.assert_any_call(
+    "Detokenization validation passed: No ERROR strings found in formatted column"
+)
+
+
+
 common.logger.info.assert_called_with("Detokenization validation passed: No ERROR strings found in formatted column")
 @patch("utils.common.upper")
 @patch("utils.common.col")
