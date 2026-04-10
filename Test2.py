79afe50976059ffba18d290da7d51414605ced05
amit9p <amit_prasad@live.com>
2026-04-11 01:01:13 +0530

    Update Test2.py

--- Test2.py
+++ Test2.py
@@ -5,7 +5,7 @@ def get_reportable_accounts(
     edq_suppressions_df: DataFrame = None,   # 👈 ADD THIS
     context: dict = None,
 ) -> DataFrame:
-
+--------
 
 from pyspark.sql import SparkSession
 from pyspark.sql import functions as F
@@ -14,3 +14,27 @@ spark = SparkSession.getActiveSession()
 
 if edq_suppressions_df is None:
     edq_suppressions_df = spark.createDataFrame([], "account_id string")
+
+
+______________
+
+
+# --- STEP 6.5: Apply EDQ Suppression (FINAL FILTER) ---
+
+edq_accounts_df = (
+    edq_suppressions_df
+    .select("account_id")
+    .dropDuplicates()
+    .withColumn("is_edq_suppressed", F.lit(True))
+)
+
+reportable_accounts_df = reportable_accounts_df.join(
+    edq_accounts_df,
+    on="account_id",
+    how="left"
+).filter(
+    F.col("is_edq_suppressed").isNull()
+)
+
+-----------
+
